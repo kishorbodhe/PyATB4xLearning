18638a8127ee129d17e5f8cbf6106aaa462f663b
Kishor Bodhe <kishorbodhe2@gmail.com>
2024-08-24 06:05:49 +0530

    20.08.2024 All files are added.

--- src/ex_20082024/Lab042.py
+++ src/ex_20082024/Lab042.py
@@ -1,3 +1,4 @@
+
 # ✅ Grade Calculator:
 # Write a program that calculates and displays the letter grade
 # for a given numerical score (e.g., A, B, C, D, or F)
@@ -18,13 +19,13 @@ score = int(input("Enter your score\n"))
 # score >=90 and scroe <=100 -> "A"
 # score >=80 and scroe <=89 -> "B"
 
-if score >= 90 and score <= 100:  # Simplified Chaining Format -> 90 <= score <= 100
+if 90 <= score <= 100:  # Simplified Chaining Format -> 90 <= score <= 100
     print("You grade is ", "A")
-elif score >= 80 and score <= 89:
+elif 80 <= score <= 89:
     print("You grade is ", "B")
-elif score >= 70 and score <= 79:
+elif 70 <= score <= 79:
     print("You grade is ", "C")
-elif score >= 60 and score <= 69:
+elif 60 <= score <= 69:
     print("You grade is ", "D")
 elif score >= 100:
     print("You are  Superman!!")
